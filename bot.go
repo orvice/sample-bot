c989f658326d4cede59e0a9657ee8b82e61e0bec
orvice <orvice@gmail.com>
2018-06-04 00:26:54 +0800

    init

---
+++ bot.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"gopkg.in/telegram-bot-api.v4"
+	"github.com/orvice/utils/env"
+)
+
+func bot() {
+	token := env.Get("Telegram_Bot_Token")
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
+}
